d41860baa9e30c4f6ae4815a374e0a18d97952e3
Misha220202 <157743520+Misha220202@users.noreply.github.com>
2024-02-23 10:06:19 -0700

    debug, fix pixel class

--- js/index.js
+++ js/index.js
@@ -4,32 +4,54 @@ const clear = document.querySelector('#CLEAR');
 function creatcanvas(x) {
     for (let i = 1; i <= x * x; i++) {
         const item = document.createElement('div');
-        item.setAttribute('class', 'pixel');
+        item.setAttribute('class', 'blank');
         item.style.width = `${(100 / x)}%`;
         item.style.height = `${(100 / x)}%`;
         item.style.backgroundColor = 'lightblue';
         container.appendChild(item);
     }
 }
-creatcanvas(25);
+
+function getpixel() {
+    x = prompt('Please enter your canvas pixel (1-100):');
+    x = Number(x);
+    if (!(Number.isInteger(x) && (x > 1 && x < 100))) {
+        alert ('Invalid input.');
+        getpixel();
+    }
+    return(x);
+}
+
+let pixel=getpixel();
+creatcanvas(pixel);
 
 const Arraycontainer = Array.from(container.children);
-
 let ismousedown = false;
+
 Arraycontainer.forEach(item => {
-    ismousedown = 'false';
     item.addEventListener('mousedown', () => {
-        ismousedown = 'true';
-        console.log(ismousedown);
+        ismousedown = true;
     });
     item.addEventListener('mousemove', () => {
-        if (ismousedown == 'true') item.style.backgroundColor = 'black';
+        if (ismousedown == true) {
+            item.style.backgroundColor = 'black';
+            item.classList.remove('blank');
+        };
     });
-    item.addEventListener('mouseup', () => ismousedown = 'false')
+    item.addEventListener('mouseup', () => ismousedown = false)
+    item.addEventListener('mouseenter', () => {
+        if (ismousedown == false) item.style.backgroundColor = 'black';
+    })
+    item.addEventListener('mouseleave', () => {
+        if (ismousedown == false && item.className == 'blank') item.style.backgroundColor = 'lightblue';
+    })
 })
 
 clear.addEventListener('click', () => {
-    Arraycontainer.forEach(item => item.style.backgroundColor = 'lightblue')
+    Arraycontainer.forEach(item => {
+        item.style.backgroundColor = 'lightblue';
+        item.setAttribute('class', 'blank');
+    })
 })
 
 
